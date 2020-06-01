b5b8953b3b15f5274290e7daa6551b56ecad8028
SAFNAS <iamsafnas@gmail.com>
2020-06-01 22:19:26 +0530

    rootquery

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express();
 
 app.use('/graphql',graphqlHTTP({
-    
+    //schema: schema
+    schema
 }))
 
 app.listen(4000, ()=>{
