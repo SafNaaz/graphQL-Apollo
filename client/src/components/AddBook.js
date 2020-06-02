9300bb515317c2bb543692a31164b100ee22acfb
SAFNAS <iamsafnas@gmail.com>
2020-06-02 18:58:08 +0530

    added both query in compose way

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { graphql } from "react-apollo";
-import {getAuthorsQuery} from '../queries/queries'
+import { graphql} from "react-apollo";
+import {getAuthorsQuery, addBookMutation} from '../queries/queries'
+import * as compose from "flowright"
 class AddBook extends Component {
 
     constructor(props){
@@ -13,7 +14,7 @@ class AddBook extends Component {
     }
 
     displayAuthors(){
-        var data = this.props.data;
+        var data = this.props.getAuthorsQuery;
 
         if (data.loading) {
             return <option disabled>Loading Authors..</option>;
@@ -26,7 +27,7 @@ class AddBook extends Component {
 
     submitForm(e){
       e.preventDefault();
-      console.log(this.state)
+      this.props.addBookMutation();
     }
 
     render() {
@@ -53,4 +54,7 @@ class AddBook extends Component {
     }
   }
 
-  export default graphql(getAuthorsQuery)(AddBook);
+  export default compose(
+    graphql(getAuthorsQuery,{name:"getAuthorsQuery"}),
+    graphql(addBookMutation,{name:"addBookMutation"})
+  )(AddBook);
