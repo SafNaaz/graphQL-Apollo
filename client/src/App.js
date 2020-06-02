85edfdd90c676099157a9146502aa12f07975208
SAFNAS <iamsafnas@gmail.com>
2020-06-02 16:23:04 +0530

    book component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+//components
+import BookList from './components/BookList'
+
 function App() {
   return (
     <div id="main">
       <h1>Ninja's Reading List</h1>
+      <BookList/>
     </div>
   );
 }
