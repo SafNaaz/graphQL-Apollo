6f4f712dee4030c7c735872a4fe5e50eae199af6
SAFNAS <iamsafnas@gmail.com>
2020-06-02 17:06:28 +0530

    rendered all the books

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-function BookList() {
-  return (
-    <div>
-      <ul id="book-list">
-          <li>Book name</li>
-      </ul>
-    </div>
-  );
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+class BookList extends Component {
+    
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading Books..</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li>{book.name}</li>;
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          {this.displayBooks()}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
