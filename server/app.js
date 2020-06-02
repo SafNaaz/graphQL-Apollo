b0b5eebfd39bb9125ba586c7234763617726a75f
SAFNAS <iamsafnas@gmail.com>
2020-06-02 12:08:26 +0530

    connected to local mongodb using mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/gql-ninja',{ useUnifiedTopology: true,useNewUrlParser: true  })
+mongoose.connection.once('open',()=>{
+    console.log(`connected to database`)
+})
 
 app.use('/graphql',graphqlHTTP({
     //schema: schema
